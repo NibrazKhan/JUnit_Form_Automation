7acbe98272fc8819b93508702d70c1b3559737c4
NibrazKhan <nibrajkhan123@gmail.com>
2022-08-11 16:35:32 +0600

    initial test

---
+++ src/test/java/JunitAutomation.java
@@ -0,0 +1,239 @@
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.*;
+
+public class JunitAutomation {
+    WebDriver driver;
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headed");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+    @Test
+    public void getTitle(){
+        driver.get("https://demoqa.com/");
+        String title= driver.getTitle();
+        Assert.assertEquals("Tools",title);
+        //Assert.assertTrue(title.contains("ToolsQA"));
+    }
+    @Test
+    public void writeText(){
+        driver.get("https://demoqa.com/text-box");
+        //driver.findElement(By.id("userName")).sendKeys("Salman");
+//        driver.findElement(By.cssSelector("[type=text]")).sendKeys("Salman");
+        //driver.findElement(By.cssSelector("[id=userName]")).sendKeys("Salman");
+        //driver.findElement(By.className("form-control")).sendKeys("Salman");
+        WebElement txtFirstName= driver.findElement(By.id("userName"));
+        txtFirstName.sendKeys("Salman");
+//        WebElement txtEmail=driver.findElement(By.xpath("//input[@id='userEmail']"));
+//        txtEmail.sendKeys("salman@test.com");
+        WebElement txtEmail=driver.findElement(By.cssSelector("[type='email']"));
+        txtEmail.sendKeys("salman@test.com");
+
+        List<WebElement> button=driver.findElements(By.tagName("button"));
+        button.get(1).click();
+
+        //driver.findElements(By.tagName("button")).get(0).click();
+    }
+    @Test
+    public void handleAlert() throws InterruptedException {
+        driver.get("https://demoqa.com/alerts");
+//        driver.findElement(By.id("alertButton")).click();
+//        Thread.sleep(3000);
+//        driver.switchTo().alert().accept();
+//        driver.findElement(By.cssSelector("[id=timerAlertButton]")).click();
+//        Thread.sleep(9000);
+//        driver.switchTo().alert().accept();
+//        driver.findElement(By.id("confirmButton")).click();
+//        Thread.sleep(2500);
+//        driver.switchTo().alert().dismiss();
+//        String text=driver.findElement(By.className("text-success")).getText();
+//        Assert.assertTrue(text.contains("Cancel"));
+        //prompt box testing
+        driver.findElement(By.id("promtButton")).click();
+        driver.switchTo().alert().sendKeys("You are life giving life");
+        Thread.sleep(3500);
+        driver.switchTo().alert().accept();
+        String text = driver.findElement(By.id("promptResult")).getText();
+        Assert.assertTrue(text.contains("life"));
+
+    }
+    @Test
+    public void selectDropDown(){
+        driver.get("https://demoqa.com/select-menu");
+        Select select=new Select(driver.findElement(By.id("oldSelectMenu")));
+        select.selectByValue("7");
+        Select cars=new Select(driver.findElement(By.id("cars")));
+        if(cars.isMultiple()){
+            cars.selectByValue("audi");
+            cars.selectByValue("volvo");
+        }
+
+
+    }
+    //modal
+    @Test
+    public void modalDialog() throws InterruptedException {
+        driver.get("https://demoqa.com/modal-dialogs");
+        driver.findElement(By.id("showSmallModal")).click();
+        Thread.sleep(3000);
+        String text=driver.findElement(By.className("modal-body")).getText();
+        System.out.println(text);
+        driver.findElement(By.id("closeSmallModal")).click();
+
+
+    }
+    @Test
+    public void mouseHover(){
+        driver.get("https://green.edu.bd/");
+        Actions action=new Actions(driver);
+        //need to create custom xpath by ourself
+        List<WebElement> list= driver.findElements(By.xpath("//a[contains(text(),\"About Us\")]"));
+        action.moveToElement(list.get(2)).perform();
+    }
+    @Test
+    public void datePicker() throws InterruptedException {
+        driver.get("https://demoqa.com/date-picker");
+        WebElement txtDate=driver.findElement(By.id("datePickerMonthYearInput"));
+        Actions action =new Actions(driver);
+//        action.moveToElement(txtDate).doubleClick().click().
+//                keyDown(Keys.BACK_SPACE).keyUp(Keys.BACK_SPACE).
+//                perform();
+        txtDate.sendKeys(Keys.CONTROL+"a");
+        txtDate.sendKeys(Keys.BACK_SPACE);
+        Thread.sleep(2000);
+        driver.findElement(By.id("datePickerMonthYearInput")).sendKeys("03/18/1998");
+        txtDate.sendKeys(Keys.ENTER);
+    }
+    @Test
+    public void keyboardEvent() throws InterruptedException {
+        driver.get("https://www.google.com/");
+        Actions action =new Actions(driver);
+        WebElement element=driver.findElement(By.name("q"));
+        action.moveToElement(element).
+                keyDown(Keys.SHIFT)
+                .sendKeys("who am i?")
+                .keyUp(Keys.SHIFT)
+                .doubleClick()
+                .click()
+                .sendKeys(Keys.BACK_SPACE)
+                .contextClick()
+                .perform();
+        Thread.sleep(5000);
+    }
+    @Test
+    public void actionClick(){
+        driver.get("https://demoqa.com/buttons");
+        List<WebElement> buttons=driver.findElements(By.cssSelector("[type=button]"));
+        Actions actions=new Actions(driver);
+        actions.doubleClick(buttons.get(1)).perform();
+        actions.contextClick(buttons.get(2)).perform();
+    }
+    @Test
+    public void takeScreenShot() throws IOException {
+        driver.get("https://www.google.com/");
+        File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        String time = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss-aa").format(new Date());
+        String fileWithPath = "./src/test/resources/screenshots/" + time + ".png";
+        File DestFile = new File(fileWithPath);
+        FileUtils.copyFile(screenshotFile, DestFile);
+    }
+    @Test
+    public void uploadFile(){
+        driver.get("https://demoqa.com/upload-download");
+        driver.findElement(By.id("uploadFile")).sendKeys("C:\\Users\\18201057\\Pictures\\Saved Pictures\\kamlesh.jpeg");
+    }
+    @Test
+    public void downloadfile(){
+        driver.get("https://demoqa.com/upload-download");
+        driver.findElement(By.id("downloadButton")).click();
+    }
+    @Test
+    public void handleTab() throws InterruptedException {
+        driver.get("https://demoqa.com/browser-windows");
+        driver.findElement(By.id("tabButton")).click();
+        Thread.sleep(3000);
+        ArrayList<String>wi=new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(wi.get(1));
+        System.out.println("New tab title:"+driver.getTitle());
+        String text=driver.findElement(By.id("sampleHeading")).getText();
+        Assert.assertTrue(text.contains("sample"));
+        driver.close();
+        driver.switchTo().window(wi.get(0));
+
+    }
+    @Test
+    public void handleWindow()throws InterruptedException{
+        driver.get("https://demoqa.com/browser-windows");
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(50));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("windowButton")));
+        driver.findElement(By.id("windowButton")).click();
+        String mainWindowHandle=driver.getWindowHandle();
+        //set used to indicate unordered elements as the window will be the child window of the main window
+        Set<String> allWindowHandles=driver.getWindowHandles();
+        Iterator<String>iterator=allWindowHandles.iterator();
+        while(iterator.hasNext()){
+            String childWindow= iterator.next();
+            if(!mainWindowHandle.equalsIgnoreCase(childWindow)){
+                driver.switchTo().window(childWindow);
+                String text=driver.findElement(By.id("sampleHeading")).getText();
+                Assert.assertTrue(text.contains("sample"));
+            }
+        }
+        driver.close();
+    }
+    @Test
+    public void scrapTable(){
+        driver.get("https://demoqa.com/webtables");
+        WebElement table= driver.findElement(By.className("rt-tbody"));
+        List<WebElement>allRows=table.findElements(By.className("rt-tr"));
+        int i=0;
+        for (WebElement row:
+             allRows) {
+            List<WebElement>cells=row.findElements(By.className("rt-td"));
+            for (WebElement cell:cells){
+                i++;
+                System.out.println("num["+i+"] "+cell.getText());
+            }
+        }
+
+    }
+    //handling Iframe
+    @Test
+    public void handleIframe(){
+        driver.get("https://demoqa.com/frames");
+        driver.switchTo().frame("frame2");
+        String text=driver.findElement(By.id("sampleHeading")).getText();
+        Assert.assertTrue(text.contains("This is a sample page"));
+        //switching back to the defaulted content
+        driver.switchTo().defaultContent();
+
+    }
+
+
+
+    @After
+    public void close() {
+//        driver.close();
+//        driver.quit();
+    }
+
+}
---
+++ src/test/java/Practice_Form.java
@@ -0,0 +1,177 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Practice_Form {
+    WebDriver driver;
+    HashMap<String,String> hashMap=new HashMap<>();
+    @Before
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headed");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+    @Test
+    public void wholeFormAutomation() throws InterruptedException, IOException, ParseException {
+        driver.get("https://demoqa.com/automation-practice-form");
+//    --------------------Header Text Match----------------------
+        String text= driver.findElement(By.tagName("h5")).getText();
+        Assert.assertEquals("Student Registration Form",text);
+//    ------------------------Form Displayed Or Not-------------------
+        WebElement displayed=driver.findElement(By.id("userForm"));
+        Assert.assertTrue(displayed.isDisplayed());
+//        --------------------Writing on Text Box Areas----------------
+        driver.findElement(By.id("firstName")).sendKeys("Nibraj");
+        driver.findElement(By.id("lastName")).sendKeys("Khan");
+        Thread.sleep(500);
+        driver.findElement(By.id("userEmail")).sendKeys("nibrajkhan123@gmail.com");
+        Thread.sleep(500);
+        driver.findElement(By.id("userNumber")).sendKeys("1780006900");
+        Thread.sleep(1500);
+        driver.findElement(By.id("currentAddress")).sendKeys("383,Rosa Vista,Modhubagh,Moghbazar");
+//    ---------------------------Radio Button Test----------------------
+        WebElement radioButtonFemale=driver.findElement(By.xpath("//label[contains(text(),'Female')]"));
+        radioButtonFemale.click();
+        //checking if the other radio button gets deselected or not after clicking another radio button.
+        WebElement radioButtonMale=driver.findElement(By.xpath("//label[contains(text(),'Male')]"));
+        Thread.sleep(1200);
+        if(!radioButtonMale.isSelected()) {
+            radioButtonMale.click();
+        }
+        System.out.println("Female radio button is deselected and Male radio button is selected");
+//        ----------------------Automating Date Picker-------------------
+        WebElement calendarInput= driver.findElement(By.id("dateOfBirthInput"));
+        calendarInput.sendKeys(Keys.CONTROL+"a");
+        Thread.sleep(1000);
+        calendarInput.sendKeys("03/18/1999");
+        Thread.sleep(1000);
+        calendarInput.sendKeys(Keys.ENTER);
+//        --------------------Subject Choose--------------
+        WebElement subjectChoose= driver.findElement(By.id("subjectsInput"));
+        subjectChoose.sendKeys("Chem");
+        Thread.sleep(1000);
+        //selecting the autocorrect value
+        subjectChoose.sendKeys(Keys.ENTER);
+        subjectChoose.sendKeys("Econom");
+        Thread.sleep(1000);
+        subjectChoose.sendKeys(Keys.ENTER);
+// ----------------- Selecting the Checkbox ----------------
+        WebElement sportsCheckBox= driver.findElement(By.id("hobbies-checkbox-1"));
+        Actions action =new Actions(driver);
+        //if first checkbox is not selected.
+        if(!sportsCheckBox.isSelected()) {
+            action.moveToElement(sportsCheckBox).click().perform();
+        }
+        WebElement readingCheckBox=driver.findElement(By.id("hobbies-checkbox-2"));
+        //if the second checkbox is enabled click on it.
+        Thread.sleep(1000);
+        if(sportsCheckBox.isEnabled()){
+            action.moveToElement(readingCheckBox).click().perform();
+        }
+//    --------------Uploading Pictures----------------
+        Thread.sleep(1000);
+        WebElement uploadPic= driver.findElement(By.id("uploadPicture"));
+        uploadPic.sendKeys("C:\\Users\\NibrazKhan\\Pictures\\Saved Pictures\\myImg.jpeg");
+//      ---------------State and City Dropdown value choose---------------
+        WebElement state= driver.findElement(By.id("react-select-3-input"));
+        WebElement city= driver.findElement(By.id("react-select-4-input"));
+        //selecting the state as Haryana by writing Ha and enter
+        //if the dropdown is enabled
+        if(state.isDisplayed()) {
+            state.sendKeys("Ha");
+              Thread.sleep(1000);
+            state.sendKeys(Keys.ENTER);
+        }
+        Thread.sleep(1000);
+        //if city is enabled
+        if(city.isEnabled()) {
+//            city.sendKeys("Pan");
+//            Thread.sleep(1000);
+//            state.sendKeys(Keys.ENTER);
+            action.moveToElement(city).click().keyDown(Keys.ENTER).keyUp(Keys.ENTER).perform();
+        }
+//        -------------- Clicking Submit Button --------------
+        WebElement submit= driver.findElement(By.id("submit"));
+        Thread.sleep(1000);
+        //clicking the submit button
+        action.moveToElement(submit).click().perform();
+        //checking whether submit button is visible or not
+//        Assert.assertTrue(submit.isDisplayed());
+//    -----------------keeping user info into json Array by extracting the info from the table-------------
+        // checking the table header message
+//        WebElement tableHeaderMsg= driver.findElement(By.className("modal-title h4"));
+//        System.out.println(tableHeaderMsg.getText());
+////        Assert.assertEquals("Thanks for submitting the form",tableHeaderMsg.getText());
+        WebElement table= driver.findElement(By.tagName("tbody"));
+        System.out.println(driver.findElement(By.tagName("tbody")).isDisplayed());
+        List<WebElement> allRows=table.findElements(By.className("tr"));
+        for (WebElement row:
+                allRows) {
+            List<WebElement>cells=row.findElements(By.className("td"));
+            hashMap.put(cells.get(0).getText(),cells.get(1).getText());
+            }
+//        String[]array=hashMap.values().toArray(new String[0]);
+//        for (String value:
+//             array) {
+//            System.out.println(value);
+//
+//        }
+        jsonDataWrite(hashMap);
+
+        }
+    public void jsonDataWrite(HashMap<String,String>userData) throws IOException, ParseException {
+        String fileName="./src/test/resources/students.json";
+        JSONParser jsonParser = new JSONParser();
+        Object obj = jsonParser.parse(new FileReader(fileName));
+        JSONObject userObj = new JSONObject();
+        for (Map.Entry<String, String> set : userData.entrySet()) {
+            userObj.put(set.getKey(), set.getValue());
+
+        }
+
+        JSONArray jsonArray = (JSONArray) obj;
+        jsonArray.add(userObj);
+        FileWriter file = new FileWriter(fileName);
+        file.write(jsonArray.toJSONString());
+        file.flush();
+        file.close();
+        System.out.println("Saved!");
+        System.out.print(jsonArray);
+
+    }
+
+
+
+    @After
+    public void close() {
+//        driver.close();
+//        driver.quit();
+    }
+
+}
